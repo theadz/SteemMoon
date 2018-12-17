61eea2c81766a44dcbf493e405060f20ed90a9e1
NinjaVideo <the_adz@hotmail.com>
2018-12-16 21:52:44 -0600

    Initial Commit
    I honestly have no idea what I'm doing.

---
+++ src/components/App.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import { Button, Jumbotron, Popover, OverlayTrigger } from 'react-bootstrap';
+import background from '../images/Moontile background.png';
+import heading from '../images/Steem Moon Title.png';
+import bear from '../images/Bear.gif';
+import './App.css';
+
+const popoverRight = (
+  <Popover id="popover-positioned-right" title="Popover right">
+    <strong>Holy guacamole!</strong> Check this info.
+  </Popover>
+);
+
+// Need to figure out how to get all of the popup data in a table so I'm not typing new stuff all the time.
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <img src={background} className="Moon-Background" alt="Moon Background"/>
+        <img src={heading} className="Steem-Moon-Title" alt="Steem Moon Title"/>
+
+        <Jumbotron>
+           <h1>Steem to the Moon!</h1>
+            <p>
+             Steem Moon is a collaborative community effort to add fun pixel art to our moonscape.
+            </p>
+            <p>
+             Hover over each piece to see who created it.
+            </p>
+            <p>
+              <Button bsStyle="primary" href="https://www.steemit.com">Instructional Post</Button>
+            </p>
+        </Jumbotron>
+        <OverlayTrigger trigger="click" placement="right" overlay={popoverRight}>
+           <img src={bear} className="Bear-contribution" alt="Bear-contribution"/>
+        </OverlayTrigger>
+      </div>
+    );
+  }
+}
+
+
+
+export default App;
