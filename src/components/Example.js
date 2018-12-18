d4db2229f994a5ba4bc64124351a366098d193e6
NinjaVideo <the_adz@hotmail.com>
2018-12-17 20:51:37 -0600

    Update Example.js
    Can't seem to delete.

--- src/components/Example.js
+++ src/components/Example.js
@@ -1,16 +1,1 @@
-
-		'    background: rgba(0,0,0,0.8);\n' +
-					'    transform: translate3d(-50%, -50%, 0);\n' +
-					'    border-radius: 5px;\n' +
-					'    pointer-events: none;\n' +
-					'    zIndex: 1000;\n' +
-					'}\n'
-				}</code></pre>
-			</div>
-		);
-	}
-});
-
-ReactDOM.render(<Example />, document.getElementById('example'));
-
-export default Example;
+// File not needed but can't seem to delete it either.
